9335a5dfd54489c979ffb4eb3119430e06b0428d
Eliad sellem <eliad.sellem@live.biu.ac.il>
2020-05-24 01:02:28 +0300

    ip port

---
+++ FlightControlWeb/test.cs
@@ -0,0 +1,23 @@
+﻿using FlightControlWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb
+{
+    public class test
+    {
+        public test()
+        {
+            string[] args = { "" };
+            var h = Program.CreateHostBuilder(args);
+            var d = new DbContextOptionsBuilder<FlightDbContext>();
+            d.UseInMemoryDatabase("moshe");
+            FlightDbContext dbContext = new FlightDbContext(d.Options);
+
+
+        }
+    }
+}
